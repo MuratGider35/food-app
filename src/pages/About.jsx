440835d20530149aa99ce134a1524331b65be054
MuratGider35 <m.gider35@gmail.com>
2022-11-04 12:24:06 +0300

    responsive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,15 +5,16 @@ const About = () => {
     <div className="about">
       <div className="murat">
         <h3>About Full-Stack Developer MURAT</h3>
-      </div>
-
-      <div className="cnt">
+        <div className="cnt">
         <p className="text-end">I'm Murat 🙂</p>
         <p>I'm currently learning Full-Stack Development Languages.</p>
         <p>
           I've already known JS, ReactJS, NodeJS, Python, Django, MongoDB, SQL.
         </p>
       </div>
+      </div>
+
+    
     </div>
   );
 };
